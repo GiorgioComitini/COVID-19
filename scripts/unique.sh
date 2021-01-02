8446fc251e0a1ff404ddbd233dea049814177e8f
GiorgioComitini <giorgiocomitini@gmail.com>
2021-01-02 21:22:01 +0100

    2021-01-02

---
+++ scripts/unique.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# This script adds the last daily update to the file cse-covid19-ita-regioni.csv
+#
+# WARNING! It adds the data in append mode, so it will result in redundant behavior
+# if used more than once a day
+
+cat "../dati-regioni/cse-covid19-ita-regioni-$(date +%Y%m%d).csv" >> "../dati-regioni/cse-covid19-ita-regioni.csv"
