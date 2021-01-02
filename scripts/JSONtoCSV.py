1099c470ea9fc8277c5f907ed56efedf8c8192e6
GiorgioComitini <giorgiocomitini@gmail.com>
2021-01-02 19:52:02 +0100

    2021-01-02

--- scripts/JSONtoCSV.py
+++ scripts/JSONtoCSV.py
@@ -3,6 +3,7 @@
 
 from sys import argv
 import json
+import cust_time as ctime
 
 fin=open("../raw-json/"+argv[1]+".json","r")
 resp=json.loads(fin.read()) # JSON data as Python dict
@@ -16,7 +17,7 @@ data+=[arr[0:2]+[arr[1]/arr[2]]+[arr[-1]]] # Adds Italy row
 
 
 fout=open("../dati-regioni/"+argv[1]+".csv","w") # Writes data as CSV file
-fout.write("data,denominazione_regione,dosi_somministrate,dosi_consegnate,percentuale_dosi_somministrate_su_consegnate\n")
+fout.write("data,denominazione_regione,dosi_somministrate,dosi_consegnate,percentuale_dosi_somministrate_su_consegnate,data_ultimo_check\n")
 for el in data:
-    fout.write(time+","+el[0]+","+str(el[1])+","+str(el[3])+","+str(round(float(el[2])*100,2))+"\n")
+    fout.write(ctime.UTCtoCET(time)+","+el[0]+","+str(el[1])+","+str(el[3])+","+str(round(float(el[2])*100,2))+","+ctime.localtime()+"\n")
 fout.close()
