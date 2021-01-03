378c2cf4d45975d7bec0fb3e24821b1938c8ae5e
GiorgioComitini <giorgiocomitini@gmail.com>
2021-01-03 16:22:01 +0100

    2021-01-03

--- scripts/unique.sh
+++ scripts/unique.sh
@@ -5,4 +5,4 @@
 # WARNING! It adds the data in append mode, so it will result in redundant behavior
 # if used more than once a day
 
-cat "../dati-regioni/cse-covid19-ita-regioni-$(date +%Y%m%d).csv" >> "../dati-regioni/cse-covid19-ita-regioni.csv"
+cat "../dati-regioni/cse-covid19-ita-regioni-$(date +%Y%m%d).csv" | tail -n +2 >> "../dati-regioni/cse-covid19-ita-regioni.csv"
